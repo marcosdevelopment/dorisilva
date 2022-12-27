0524306b65e14e4a91fc9150ee20bce803f50299
Marcos <marcoscesar.rochadevelopment@gmail.com>
2022-12-27 03:09:13 -0300

    adicionado o componente carousel

--- src/components/propertieinfo/PropertieInfo.js
+++ src/components/propertieinfo/PropertieInfo.js
@@ -1,23 +1,41 @@
 import "./PropertieInfo.Module.css"
-
-const PropertieInfo = ({type, 
-  district, 
-  city, 
-  state, 
-  rooms, 
-  bedrooms, 
-  restrooms, 
-  livingroom, 
-  kitchen, 
-  garage, 
-  externalArea, 
-  size, 
-  observation,
-  price
-}) => {
+import PropertiesData from "../../data/PropertiesData"
+const PropertieInfo = ({id}) => {
   return (
     <div className="container_propertieInfo">
-      <p><span>Tipo:</span> {type}</p>
+      {PropertiesData.filter(properties => properties.id === id)
+        .map(propertieFilter => (
+          <div>
+            <p><span>Tipo:</span> {propertieFilter.type}</p>
+            <hr/>
+            <p><span>Bairro:</span> {propertieFilter.district}</p>
+            <hr/>
+            <p><span>Cidade:</span> {propertieFilter.city}</p>
+            <hr/>
+            <p><span>Estado:</span> {propertieFilter.state}</p>
+            <hr/>
+            <p><span>Comôdos:</span> {propertieFilter.rooms}</p>
+            <hr/>
+            <p><span>Quartos:</span> {propertieFilter.bedrooms}</p>
+            <hr/>
+            <p><span>Banheiros:</span> {propertieFilter.restrooms}</p>
+            <hr/>
+            <p><span>Sala:</span> {propertieFilter.livingroom}</p>
+            <hr/>
+            <p><span>Cozinha:</span> {propertieFilter.kitchen}</p>
+            <hr/>
+            <p><span>Garagem:</span> {propertieFilter.garage}</p>
+            <hr/>
+            <p><span>Área externa:</span> {propertieFilter.externalArea}</p>
+            <hr/>
+            <p><span>Tamanho:</span> {propertieFilter.size}</p>
+            <hr/>
+            <p className="propertieInfo_obs">{propertieFilter.observation}</p>
+            <h2 className="propertieInfo_price">R$ {propertieFilter.price}</h2> 
+          </div>
+        ))
+      }
+      {/* <p><span>Tipo:</span> {type}</p>
       <hr/>
       <p><span>Bairro:</span> {district}</p>
       <hr/>
@@ -42,7 +60,7 @@ const PropertieInfo = ({type,
       <p><span>Tamanho:</span> {size}</p>
       <hr/>
       <p className="propertieInfo_obs">{observation}</p>
-      <h2 className="propertieInfo_price">R$ {price}</h2>
+      <h2 className="propertieInfo_price">R$ {price}</h2> */}
     </div>
   )
 }
